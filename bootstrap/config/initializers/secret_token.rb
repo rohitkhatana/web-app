aaed51ea639535b6eee1a31cf1d36108a9b4db14
rohit khatana <rohitkhatana.khatana@gmail.com>
2013-12-16 04:26:00 +0530

    adding rails aaplication

---
+++ bootstrap/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bootstrap::Application.config.secret_token = 'ec1ea3424268ca25f272948df213dcc4462ae2b54656040b8b451a02c42a6b1cd00628cd81b119d684d8332ac43a405149dd8839fdc660483288adb30ee687e5'
---
+++ store/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Store::Application.config.secret_token = '85567c800229beb2c56a72349beb09834a6aac928809e704ac525c3639fd980627a694b084adf59ca9e8ea590ac580ae20914cec2e4d93d6f43c847fbe1a3806'
