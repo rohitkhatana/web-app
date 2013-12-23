5facfa3fdb5c4053dade42056f91030e4d057e91
rohit khatana <rohitkhatana.khatana@gmail.com>
2013-12-23 10:22:05 +0530

    adding mally

---
+++ Twitance/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Twitance::Application.config.secret_token = '65abca9bd96bc888f43e8a2bb2be738b28a57710403782503b494facbb2eb571fc7a08d75c65424f4b93d881a72bd9deb63bddbf2009aa4c6245962e50a05390'
